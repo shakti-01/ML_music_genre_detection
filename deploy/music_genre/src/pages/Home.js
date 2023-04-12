0c157d8b47da62b3b76805fd461dec12a296733b
Shakti Prasad Nanda <pnandashakti@gmail.com>
2023-04-12 22:06:41 +0530

    server added

--- deploy/music_genre/src/pages/Home.js
+++ deploy/music_genre/src/pages/Home.js
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
+  const [file, setFile] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (file == null) {
+      alert('Please select a file to upload');
+      return;
+    }
+    const data = new FormData();
+    data.append('audio-file', file);
+    const response = await fetch('http://localhost:5000/upload', {
+      method: 'POST',
+      body: data
+    });
+    const res = await response.json();
+    console.log(res);
+
+    if (!res.success) {
+      alert("Give valid audio file");
+    }
+    else {
+      alert('nice')
+    }
+  }
+  const onChange = (e) => {
+    if (e.target.files) setFile(e.target.files[0]);
+  }
   return (
     <div className='home'>
-        <h1>Music genre detection system</h1>
-        <hr/>
-        <div className='home-main'>
-            <form>
-                <input type='file' placeholder='select music'/>
-                <br/>
-                <button type='submit'>Find genre</button>
-            </form>
-            <br/>
-            <div className='result'>This belongs to hip-hop</div>
-        </div>
+      <h1>Music genre detection system</h1>
+      <hr />
+      <div className='home-main'>
+        <form onSubmit={handleSubmit} encType='multipart/form-data'>
+          <input type='file' onChange={onChange} />
+          <br />
+          <button type='submit'>Find genre</button>
+        </form>
+        <br />
+        <div className='result'>This belongs to hip-hop</div>
+      </div>
     </div>
   )
 }
