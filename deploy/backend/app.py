18e1ea3e9c6797c488cda247400d7043e4154c80
Shakti Prasad Nanda <pnandashakti@gmail.com>
2023-04-17 19:56:17 +0530

    working model done

--- deploy/backend/app.py
+++ deploy/backend/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, redirect
+from flask import Flask, jsonify
 from flask import request
 import os
 from flask_cors import CORS, cross_origin
 import numpy as np
 import tensorflow as tf
 import librosa
+import math
 
 app = Flask(__name__)
 CORS(app)
@@ -12,32 +13,38 @@ CORS(app)
 #global var
 genre={}
 genre[0] = 'Blues';genre[1]='Classical';genre[2]='Country';genre[3]='Disco';genre[4]='Hip-hop';genre[5]='Jazz';
-genre[6]= 'Metal';genre[7]='PoP';genre[8]='Reggae';genre[9]='Rock'
+genre[6]= 'Metal';genre[7]='Pop';genre[8]='Reggae';genre[9]='Rock'
 loaded_model = tf.keras.models.load_model(app.instance_path+'/static/my_music_model_final')
 print('model loaded successfully')
 
 def getMFCC(audio_path):
-    # Load audio file
-    #audio_path = '/content/drive/MyDrive/Music Genre Classification/genres_original/pop/pop.00008.wav'
-    y, sr = librosa.load(audio_path, sr=22050, duration=30)
+    y, sr_y=librosa.load(audio_path)
 
-    # Extract MFCC features
-    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, hop_length=int(sr*0.010), n_fft=int(sr*0.025))
+    SAMPLE_RATE=22050
+    DURATION=30
+    SAMPLES_PER_TRACK=SAMPLE_RATE*DURATION
+    n_fft=2048 #int(SAMPLE_RATE*0.025)
+    hop_length=512 #int(SAMPLE_RATE*0.010)
+    num_segments=10
+    num_samples_per_segment=int(SAMPLES_PER_TRACK/num_segments)
+    expected_num_mfcc_vectors_per_segment =math.ceil(num_samples_per_segment/hop_length)
 
-    # Reshape MFCC features into (130,13) tensor
-    if mfcc.shape[1] < 130:
-        # Pad with zeros if number of frames is less than 130
-        mfcc = np.pad(mfcc, ((0, 0), (0, 130-mfcc.shape[1])), mode='constant')
-    elif mfcc.shape[1] > 130:
-        # Truncate frames if number of frames is greater than 130
-        mfcc = mfcc[:, :130]
-    else:
-        # Keep the same shape if number of frames is exactly 130
-        pass
+    data={'mfcc':[]}
+    for s in range(num_segments):
+        start_sample = num_samples_per_segment * s
+        finish_sample= start_sample + num_samples_per_segment
+        mfcc = librosa.feature.mfcc(y=y[start_sample:finish_sample], 
+                                    sr=SAMPLE_RATE, 
+                                    n_fft=n_fft,
+                                    hop_length=hop_length,
+                                    n_mfcc=13
+                                    )
+        mfcc=mfcc.T
+        if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+            data['mfcc'].append(mfcc.tolist())
 
-    # Reshape MFCC features into (1, 130, 13) tensor for input to RNN-LSTM model
-    mfcc = mfcc.reshape(1, mfcc.shape[1], mfcc.shape[0])
-    return mfcc
+
+    return np.array(data['mfcc'][0]).reshape(1,130,13)
 
 @app.route('/')
 def home():
--- deploy/music_genre/README.md
+++ deploy/music_genre/README.md
@@ -68,3 +68,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### how to run on a different machine?
+install dependencies: 
+> pip install -r requirements.txt (flask)
+> npm i
