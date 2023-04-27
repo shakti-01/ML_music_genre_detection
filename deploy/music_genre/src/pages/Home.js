461c2e333ff754fe58cc0f01a52fd6fbcdf0fa1c
Shakti Prasad Nanda <pnandashakti@gmail.com>
2023-04-27 18:30:02 +0530

    recomend added

--- deploy/music_genre/src/pages/Home.js
+++ deploy/music_genre/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState,useRef, useEffect } from 'react'
 
 function Home() {
   const [file, setFile] = useState(null);
+  const [rec,setRec] = useState(false);
   const result_genre = useRef();
   useEffect(() => {
     result_genre.current.innerText = "Please select a file to upload";
@@ -28,7 +29,21 @@ function Home() {
     }
     else {
       result_genre.current.innerText = "This belongs to "+res.genre+"\nthis is predicted with a confidence of "+res.confidence;
-      
+      //show recomendations
+      let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=0,height=0,left=-1000,top=-1000`;
+      if(rec){
+        if(res.genre === 'Jazz')window.open("https://youtu.be/cd_YsXGYsbo",'test',params);
+        else if(res.genre === 'Blues')window.open("https://youtu.be/myaUdTT6MV8",'test',params);
+        else if(res.genre === 'Classical')window.open("https://youtu.be/_4IRMYuE1hI",'test',params);
+        else if(res.genre === 'Country')window.open("https://youtu.be/ftoJ6nqTcbg",'test',params);
+        else if(res.genre === 'Disco')window.open("https://youtu.be/Gs069dndIYk",'test',params);
+        else if(res.genre === 'Hip-hop')window.open("https://youtu.be/PQZhN65vq9E",'test',params);
+        else if(res.genre === 'Metal')window.open("https://youtu.be/hTWKbfoikeg",'test',params);
+        else if(res.genre === 'Pop')window.open("https://youtu.be/kJQP7kiw5Fk",'test',params);
+        else if(res.genre === 'Reggae')window.open("https://youtu.be/z4ScbuTIbgM",'test',params);
+        else if(res.genre === 'Rock')window.open("https://youtu.be/15qAby5CzBQ",'test',params);
+      }
+
     }
   }
   const onChange = (e) => {
@@ -46,6 +61,8 @@ function Home() {
         </form>
         <br />
         <div className='result' ref={result_genre}>blank</div>
+        <div><label>Do you want recomendations based on the precdiction?</label><input type='checkbox' checked={rec} onChange={e=>{setRec(!rec)}}></input></div>
+      <img src="./model-arc.jpeg" alt="pic-model"/>
       </div>
     </div>
   )
